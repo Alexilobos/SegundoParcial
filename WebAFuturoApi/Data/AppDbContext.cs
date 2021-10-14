ad4908b98854efee505db0db9d3f7f81ab8ac16c
Alexi Lobos <a2020117797@estudiantes.upsa.edu.bo>
2021-10-13 21:00:27 -0400

    Directorio Data creado y implementado del AppDbContext

---
+++ WebAFuturoApi/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAFuturoApi.Models;
+
+namespace WebAFuturoApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Futuro> Futuro { get; set; }
+    }
+
+}
